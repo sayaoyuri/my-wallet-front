fe15a23c69eed6ad161103275d083b47600ec5a4
sayaoyuri <yuriantunes0000@gmail.com>
2023-07-05 10:56:09 -0300

    Add Sign Up request to /cadastro endpoint

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,18 +1,38 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react";
 
 export default function SignInPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  console.log(email, password);
+
+  function signIn() {
+    
+  }
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={signIn}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
+        <input 
+          placeholder="E-mail" type="email"
+          required
+          onChange={(ev) => setEmail(ev.target.value)} 
+          value={email}
+        />
+        <input 
+          placeholder="Senha" type="password" 
+          autocomplete="new-password" 
+          required
+          onChange={(ev) => setPassword(ev.target.value)}
+          value={password}
+        />
         <button>Entrar</button>
       </form>
 
-      <Link>
+      <Link to={'/cadastro'}>
         Primeira vez? Cadastre-se!
       </Link>
     </SingInContainer>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,20 +1,67 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
+import { useState } from "react";
+import axios from "axios";
 import MyWalletLogo from "../components/MyWalletLogo"
 
 export default function SignUpPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  function signUp (ev) {
+    ev.preventDefault();
+    if(password !== confirmPassword) return alert('As senhas digitadas não correspondem');
+
+    const userData = { name, email, password };
+
+    axios.post(`${import.meta.env.VITE_API_URL}/cadastro`, userData)
+      .then((res) => { alert(res.data); navigate('/') })
+      .catch((e) => alert(e.response.data));
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={signUp}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
+        <input 
+          placeholder="Nome" 
+          type="text" 
+          required 
+          onChange={(ev) => setName(ev.target.value)} 
+          value={name}
+        />
+        <input 
+          placeholder="E-mail" 
+          type="email" 
+          required 
+          onChange={(ev) => setEmail(ev.target.value)} 
+          value={email}
+        />
+        <input 
+          placeholder="Senha" 
+          type="password" 
+          autocomplete="new-password" 
+          minLength={3}
+          required 
+          onChange={(ev) => setPassword(ev.target.value)} 
+          value={password}
+          />
+        <input 
+          placeholder="Confirme a senha"
+          type="password"
+          minLength={3}
+          required
+          onChange={(ev) => setConfirmPassword(ev.target.value)}
+          value={confirmPassword}
+        />
         <button>Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to={'/'}>
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
